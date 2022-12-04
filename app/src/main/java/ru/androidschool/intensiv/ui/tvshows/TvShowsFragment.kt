30e041629c6c6e5718bc0e137733e8934c2b5425
Sergey Korchagin <kor.work@mail.ru>
2022-12-04 19:01:28 +0800

    Initial commit/ Home work 1

--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -3,7 +3,6 @@ package ru.androidschool.intensiv.ui.tvshows
 import androidx.fragment.app.Fragment
 import ru.androidschool.intensiv.R
 
-class TvShowsFragment : Fragment(R.layout.tv_shows_fragment)
-{
+class TvShowsFragment : Fragment(R.layout.tv_shows_fragment) {
 ////////новый файллйцаопsdf
 }
---
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsItem.kt
@@ -0,0 +1,33 @@
+package ru.androidschool.intensiv.ui.tvshows
+
+import android.view.View
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.viewbinding.BindableItem
+import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.Movie
+import ru.androidschool.intensiv.databinding.TvShowsItemBinding
+
+class TvShowsItem(
+    private val content: Movie,
+    private val onClick: (muvie: Movie) -> Unit
+) : BindableItem<TvShowsItemBinding>() {
+    override fun bind(viewBinding: TvShowsItemBinding, position: Int) {
+        viewBinding.tvShowDescription.text = content.title
+        viewBinding.tvShowMovieRating.rating = content.rating
+        viewBinding.tvShowItemContent.setOnClickListener {
+            onClick.invoke(content)
+        }
+
+        Picasso.get()
+            .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhY" +
+                    "jQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
+            .into(viewBinding.tvShowImagePreview)
+    }
+
+    override fun getLayout(): Int = R.layout.tv_shows_item
+
+    override fun initializeViewBinding(view: View) = TvShowsItemBinding.bind(view)
+
+
+
+}
