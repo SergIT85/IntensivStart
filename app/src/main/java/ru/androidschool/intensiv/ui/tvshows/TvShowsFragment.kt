4ab65142a5c815f7789d2b698c8f0af06453600c
Sergey Korchagin <kor.work@mail.ru>
2022-12-04 17:22:58 +0800

    Initial commit/ Home work 1
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1
    
    Initial commit/
    Home work 1. attempt 3.

--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -1,11 +1,63 @@
 package ru.androidschool.intensiv.ui.tvshows
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.MockRepository
+import ru.androidschool.intensiv.data.Movie
+import ru.androidschool.intensiv.databinding.TvShowsFragmentBinding
 
-class TvShowsFragment : Fragment(R.layout.tv_shows_fragment)
-{
+class TvShowsFragment : Fragment(R.layout.tv_shows_fragment) {
+    private var _binding: TvShowsFragmentBinding? = null
+    private val binding get() = _binding!!
 
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = TvShowsFragmentBinding.inflate(inflater, container, false)
+        return  binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listTvShow = MockRepository.getMovies().map {
+            TvShowsItem(it) { movie ->
+            openMovieDrtails(movie)
+            }
+        }.toList()
+
+        binding.tvShowsRecyclerView.adapter = adapter.apply { addAll(listTvShow) }
+    }
+
+    private val options = navOptions {
+        anim {
+            enter = R.anim.slide_in_right
+            exit = R.anim.slide_out_left
+            popEnter = R.anim.slide_in_left
+            popExit = R.anim.slide_out_right
+        }
+    }
+
+    private fun openMovieDrtails(movie: Movie) {
+        val bundle = Bundle()
+        bundle.putString(KEY_TITLE, movie.title)
+        findNavController().navigate(R.id.movie_details_fragment, bundle, options)
+    }
+    companion object {
+        const val KEY_TITLE = "title"
+    }
 }
